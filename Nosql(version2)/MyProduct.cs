96ec2c7678b72f82564e2445fc487146ea9cff94
LetiLubov <lubovruzanova@gmail.com>
2016-12-23 18:45:59 +0300

    Add files via upload

---
+++ Nosql(version2)/MyProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class MyProduct
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Price { get; set; }
+    public string Country { get; set; }
+}
